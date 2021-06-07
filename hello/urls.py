79bba1dc6c1d1262761aecedea96849bd35dd660
prateeksahu <prateeksahu0101@gmail.com>
2021-06-08 00:22:14 +0530

    new commit

--- hello/urls.py
+++ hello/urls.py
@@ -1,7 +1,7 @@
-"""hello URL Configuration
+"""Hello URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,13 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
-admin.site.site_header = "DataFit Admin"
-admin.site.site_title = "DataFit Admin Portal"
-admin.site.index_title = "Welcome to Datafit"
+admin.site.site_header = "Harry Ice Cream Admin"
+admin.site.site_title = "Harry Ice Cream Admin Portal"
+admin.site.index_title = "Welcome to Harry Ice Creams"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('home.urls'))
+    path('', include('home.urls'))
+
 ]
